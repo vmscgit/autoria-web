f5c9b6a7db6eac461c4eb57f5c847f9855c085e1
Vitor Miguel <vitor.miguel1@escolar.ifrn.edu.br>
2026-09-03 17:17:18 +0000

    corrige id da mensagem no HTML e ajusta a lógica do JavaScript; altera cor de fundo da classe destaque no CSS

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
-const mensagem = document.getElementById('Menssagem');
+console.log("O JavaScript está funcionando corretamente!");
 
-const botao = document.getElementById('botao');
-const remover = document.getElementById('remover');
-const alterar = document.getElementById('alterar');
-const alterarTitulo = document.getElementById('AlterarTitulo');
-const aviso = document.getElementById('aviso');
-const modoEscuro = document.getElementById('modoEscuro');
+const titulo = document.getElementById("titulo");
+const mensagem = document.getElementById("mensagem");
+const botao = document.getElementById("botao");
+const remover = document.getElementById("remover");
+const alterar = document.getElementById("alterar");
 
-botao.addEventListener('click', () => {
-  mensagem.classList.add('destaque');
+botao.addEventListener("click", function () {
+    mensagem.classList.add("destaque");
 });
 
-remover.addEventListener('click', () => {
-  mensagem.classList.remove('destaque', 'aviso');
-});
-
-alterar.addEventListener('click', () => {
-  mensagem.textContent = 'Status alterado com sucesso!';
-});
-
-alterarTitulo.addEventListener('click', () => {
-  document.title = 'DOM alterado';
-});
-
-aviso.addEventListener('click', () => {
-  mensagem.classList.add('aviso');
-});
-
-modoEscuro.addEventListener('click', () => {
-  document.body.classList.toggle('escuro');
+remover.addEventListener("click", function () {
+    mensagem.classList.toggle("destaque");
 });
