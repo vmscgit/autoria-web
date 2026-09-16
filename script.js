a256b630fb709d7631fcb1208cb56dea6445a92f
Vitor Miguel <vitor.miguel1@escolar.ifrn.edu.br>
2026-09-16 19:59:01 +0000

    atualiza o idioma do HTML para pt-BR, corrige títulos e adiciona funcionalidades de alterar título e aplicar aviso no JavaScript

--- script.js
+++ script.js
@@ -5,11 +5,31 @@ const mensagem = document.getElementById("mensagem");
 const botao = document.getElementById("botao");
 const remover = document.getElementById("remover");
 const alterar = document.getElementById("alterar");
+const alterarTitulo = document.getElementById("alterarTitulo");
+const aviso = document.getElementById("aviso");
+const modoEscuro = document.getElementById("modoEscuro");
 
 botao.addEventListener("click", function () {
     mensagem.classList.add("destaque");
 });
 
 remover.addEventListener("click", function () {
-    mensagem.classList.toggle("destaque");
+    mensagem.classList.remove("destaque");
+});
+
+alterar.addEventListener("click", function () {
+    mensagem.textContent = "Status alterado com sucesso!";
+});
+
+alterarTitulo.addEventListener("click", function () {
+    titulo.textContent = "DOM atualizado";
+    document.title = "DOM atualizado";
+});
+
+aviso.addEventListener("click", function () {
+    mensagem.classList.toggle("aviso");
+});
+
+modoEscuro.addEventListener("click", function () {
+    document.body.classList.toggle("escuro");
 });
